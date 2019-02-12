7ddbd3db19e85e44a91b4c4851543225a3ca4d39
pokutuna <popopopopokutuna@gmail.com>
2019-02-12 18:47:17 +0900

    jsonschema で spreadsheet の内容をバリデーションする

--- gasp/spreadsheet.py
+++ gasp/spreadsheet.py
@@ -2,12 +2,14 @@ import logging
 
 import googleapiclient.discovery as discovery
 from google.oauth2 import service_account
+from jsonschema import validate
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
-
-# values = sheet.values().get(spreadsheetId=config.get('spreadsheet.id'), range="Order").execute()
+# jsonschema parts
+non_empty_string = {"type": "string", "minLength": 1}
+positive_integer = {"type": "integer", "minimum": 1}
 
 
 class Spreadsheet:
@@ -20,7 +22,113 @@ class Spreadsheet:
 
         self.config = config
         self.api = service.spreadsheets()
-        self.spreadsheetId = config.get("spreadsheet.id")
+        self.getopts = {
+            "spreadsheetId": config.get("spreadsheet.id"),
+            "majorDimension": "ROWS",
+            "valueRenderOption": "UNFORMATTED_VALUE",
+        }
+        self.cache = {}
 
-    def check_configs(self):
-        pass
+    def fetch_rows(self, sheet_type):
+        if sheet_type in self.cache:
+            return self.cache[sheet_type]
+
+        sheet_config = self.config.get_or_default(f"spreadsheet.sheets.{sheet_type}")
+        assert sheet_type in self.config.get_or_default("spreadsheet.sheets").keys()
+
+        rows = []
+
+        hasNext, page, rows_per_req = True, 0, 1000
+        while hasNext:
+            start, end = page * rows_per_req + 1, (page + 1) * rows_per_req
+            range = f"'{sheet_config['sheet_name']}'!A{start}:R{end}"
+
+            logger.info(f"fetching a sheet: {range}")
+            response = self.api.values().get(range=range, **self.getopts).execute()  # check failure
+            rows.extend(response["values"])
+            page += 1
+            if len(response["values"]) < rows_per_req:
+                hasNext = False
+
+        # filter empty & comment
+        rows = list(filter(lambda r: len(r) != 0 and not r[0].startswith("#"), rows))
+
+        # rows to dict
+        header, mapping = rows.pop(0), {}
+        for key, name in sheet_config["columns"].items():
+            mapping[key] = header.index(name)
+        models = map(lambda r: {k: r[idx] if idx < len(r) else "" for k, idx in mapping.items()}, rows)
+
+        self.cache[sheet_type] = list(models)
+        return self.cache[sheet_type]
+
+    def check_settings(self):
+        logger.info(f"check settings in the spreadsheet")
+
+        orders = self.fetch_rows("order")
+        self.check_orders(orders)
+        lineitems = self.fetch_rows("lineitem")
+        self.check_lineitems(lineitems, orders)
+        creatives = self.fetch_rows("creative")
+        self.check_creatives(creatives, orders, lineitems)
+
+    def check_orders(self, orders):
+        # check values
+        schema = {
+            "type": "object",
+            "properties": {
+                "name": non_empty_string,
+                "advertiser_name": non_empty_string,
+                "trafficker_name": non_empty_string,
+            },
+            "required": ["name", "advertiser_name", "trafficker_name"],
+        }
+        [validate(o, schema) for o in orders]
+
+        # name must be unique
+        names = list(map(lambda o: o["name"], orders))
+        assert len(set(names)) == len(names), "order name must be unique"
+
+    def check_lineitems(self, lineitems, orders):
+        order_names = list(set(list(map(lambda o: o["name"], orders))))
+
+        single_keyvalue = {"type": "string", "pattern": "^(.+=.+)?$"}
+
+        schema = {
+            "type": "object",
+            "properties": {
+                "order_name": {"type": "string", "enum": order_names},
+                "name": non_empty_string,
+                "sizes": {"type": "string", "minLength": 1, "pattern": "^\\d+x\\d+(\\s?,\\s?\\d+x\\d+)*$"},
+                "costPerUnit": positive_integer,
+                "targetingUnit": positive_integer,
+                "targetingKeyValue1": single_keyvalue,
+                "targetingKeyValue2": single_keyvalue,
+                "targetingKeyValue3": single_keyvalue,
+            },
+            "required": ["order_name", "name", "sizes", "costPerUnit", "targetingUnit"],
+        }
+        [validate(l, schema) for l in lineitems]
+
+        # name must be unique in order
+        names = list(map(lambda li: li["name"] + li["order_name"], lineitems))
+        assert len(set(names)) == len(names), "lineitem name must be unique in order"
+
+    def check_creatives(self, creatives, orders, lineitems):
+        order_names = list(set(list(map(lambda o: o["name"], orders))))
+        lineitem_names = list(set(list(map(lambda li: li["name"], lineitems))))
+
+        schema = {
+            "type": "object",
+            "properties": {
+                "order_name": {"type": "string", "enum": order_names},
+                "lineitem_name": {"type": "string", "enum": lineitem_names},
+                "name": non_empty_string,
+                "snippet": non_empty_string,
+            },
+        }
+        [validate(c, schema) for c in creatives]
+
+        # name must be unique
+        names = list(map(lambda c: c["name"], creatives))
+        assert len(set(names)) == len(names), "creative name must be unique"
